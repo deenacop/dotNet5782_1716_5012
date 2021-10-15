24cbc65a410e10e0cfc1eea4c88106cf9754c3cd
שירה סגל <shisegal@g.jct.ac.il>
2021-10-15 10:00:07 +0300

    classes

---
+++ DAL/Drone.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        /// <summary>
+        /// Drone details
+        /// </summary>
+        public struct Drone
+        {
+            public int ID { get; set; }
+            public string Model { get; set; }
+            public WeightCategories Weight { get; set; }
+            public DroneStatus Status { get; set; }
+            public double Battery { get; set; }
+            public Priorities Priority { get; set; }
+        }
+    }
+}
