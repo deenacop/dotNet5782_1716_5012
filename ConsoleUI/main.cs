64ede57a9c124ccc882ea404744e4b0ddd819b32
שירה סגל <shisegal@g.jct.ac.il>
2021-10-20 12:13:03 +0300

    merge

--- ConsoleUI/main.cs
+++ ConsoleUI/main.cs
@@ -19,8 +19,50 @@ namespace ConsoleUI
             {
                 case 1:
                     UserChooseAddItem();
-                   
-                    
+                    Console.WriteLine("*station name");
+                    NewStation.Name = Console.ReadLine(); Console.WriteLine("For adding a base station Type 1." +
+                        "\nTo add a drone, type 2." +
+                        "\nTo receive a new customer, type 3." +
+                        "\nTo receive a new sabotage, type 4.");
+                    UserAnswer = Console.Read();
+                    switch (UserAnswer)
+                    {
+                        case 1:
+                            {
+                                IDAL.DO.Station NewStation = new IDAL.DO.Station();
+                                Console.WriteLine("Enter the new station details: *station ID (4 digits)");
+                                NewStation.ID = Console.Read();
+                                Console.WriteLine("*station name");
+                                NewStation.Name = Console.ReadLine();
+                                Console.WriteLine("*station number of available charge slots");
+                                NewStation.NumOfAvailableChargeSlots = Console.Read();
+                                Console.WriteLine("*station longitude");
+                                NewStation.Longitude = Console.Read();
+                                Console.WriteLine("*station latitude");
+                                NewStation.Latitude = Console.Read();
+                                IDAL.DO.DalObject.Add(NewStation);
+                                break;
+                            }
+                        case 2:
+                            {
+                                IDAL.DO.Station NewStation = new IDAL.DO.Station();
+                                Console.WriteLine("Enter the new station details: *station ID (4 digits)");
+                                NewStation.ID = Console.Read();
+                                Console.WriteLine("*station name");
+                                NewStation.Name = Console.ReadLine();
+                                Console.WriteLine("*station number of available charge slots");
+                                NewStation.NumOfAvailableChargeSlots = Console.Read();
+                                Console.WriteLine("*station longitude");
+                                NewStation.Longitude = Console.Read();
+                                Console.WriteLine("*station latitude");
+                                NewStation.Latitude = Console.Read();
+                                IDAL.DO.DalObject.Add(NewStation);
+                                break;
+                            }
+                    }
+                    break;
+
+
             }
         }
     }
