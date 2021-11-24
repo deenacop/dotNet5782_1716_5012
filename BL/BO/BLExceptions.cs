df6e73a11a79b9a34d90a88525618ce4d302f9d4
שירה סגל <shisegal@g.jct.ac.il>
2021-11-24 23:35:14 +0200

    TryParsw new exeption

--- BL/BO/BLExceptions.cs
+++ BL/BO/BLExceptions.cs
@@ -87,4 +87,16 @@ namespace IBL.BO
             return Message;
         }
     }
+    [Serializable]
+    public class WrongInputException : Exception
+    {
+        public WrongInputException() : base() { }
+        public WrongInputException(string message) : base(message) { }
+        public WrongInputException(string message, Exception inner) : base(message, inner) { }
+        public override string ToString()
+        {
+            return Message;
+        }
+    }
+
 }
